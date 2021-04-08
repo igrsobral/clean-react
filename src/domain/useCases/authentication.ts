0c742c13a6caab4b48c51277e9b9f3193cdd86e9
igorWebMASTER <igor_1917@hotmail.com>
2021-04-08 17:02:15 -0300

    feat: add authentication usecase

---
+++ src/domain/useCases/authentication.ts
@@ -0,0 +1,12 @@
+import { AccountModel} from '../models/accout-models'
+
+type AuthenticationParams = {
+  email: string
+  password: string
+}
+
+export interface Authentication {
+    auth(params: AuthenticationParams): Promise<AccountModel>{
+
+    }
+}
