985c64f14801d0ca161f0ba4284ade47682e2b75
igorWebMASTER <igor_1917@hotmail.com>
2022-02-09 23:59:01 -0300

    chore: add configuration of webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,43 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('webpack-cleanup-plugin');
+const { DefinePlugin } = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common')
 
-module.exports = {
-    mode: 'development',
-    entry: './src/main/index.tsx',
-    output: {
-        path: path.join(__dirane, 'public/js'),
-        publicPath: '/public/js',
-        filename: 'bundle.js',
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', 'scss', 'css'],
-        alias: {
-            '@': path.join(__dirname, 'src'),
+module.exports = merge(common, {
+  mode: 'development',
+  module: {
+    rules: [{
+      test: /\.ts(x?)$/,
+      loader: 'ts-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.scss$/,
+      use: [{
+        loader: 'style-loader'
+      }, {
+        loader: 'css-loader',
+        options: {
+          modules: true
         }
+      }, {
+        loader: 'sass-loader'
+      }]
+    }]
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    devMiddleware: {
+      writeToDisk: true
     },
-    module: {
-        rules: [
-            {
-                test: /\.ts(x?)$/,
-                loader: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.(s?)css$/,
-                use: [{
-                    loader: 'style-loader',
-                }, {
-                    loader: 'css-loader',
-                    options: {
-                        modules: true
-                    },
-                }, {
-                    loader: 'sass-loader',
-                }]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader',
-                ],
-            },
-
-        ],
+    static: {
+      directory: './public'
     },
-
-    devServer: {
-        contentBase: 'public',
-        writeToDisk: true,
-        historyApiFallback: true
-    },
-    externals: {
-        react: 'React',
-        'react-dom': 'ReactDOM',
-    },
-    plugins: [
-        new CleanWebpackPlugin()
-    ]
-}
+    historyApiFallback: true,
+    port: 8080
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './template.dev.html'
+    })
+  ]
+})
