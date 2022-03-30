43ec9e8d6e42d4f38a93bb58809ddf6750bb8eb1
igor <igor.sobral@pagtel.com.br>
2022-03-29 22:59:11 -0300

    test: ensure Login calls validation with correct password

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -51,5 +51,14 @@ describe('Login component', () => {
         });
     });
 
+    test('Should call Validation with correct password', () => {
+        const { sut, validationSpy } = makeSut();
+        const passwordInput = sut.getByTestId('password');
+        fireEvent.input(passwordInput, { target: { value: 'any_password' }});
+        expect(validationSpy.input).toEqual({
+            password: 'any_password'
+        });
+    });
+
     
 })
