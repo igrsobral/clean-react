afaf41ba4d18779dd2608a2a5c1b32ed70c6633f
Igor Ribeiro <igor_1917@hotmail.com>
2022-05-15 15:40:08 -0300

    test: ensure SignUp enable submit button if form is valid

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -89,4 +89,14 @@ describe('SignUp component', () => {
         Helper.testStatusForField(sut, 'passwordConfirmation');
     });
 
+    test('Should enable submit button if form is valid', () => {
+        const { sut } = makeSut();
+        Helper.populateField(sut, 'name');
+        Helper.populateField(sut, 'email');
+        Helper.populateField(sut, 'password');
+        Helper.populateField(sut, 'passwordConfirmation');
+        Helper.testButtonIsDisabled(sut, 'submit', false);
+    });
+
+
 });
