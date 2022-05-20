6d19ed028f716522344e05ea8c00cf427e2dc480
Igor Ribeiro <igor_1917@hotmail.com>
2022-05-19 23:51:33 -0300

    test: ensure SignUp present error if SaveAccessToken fails

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -151,7 +151,7 @@ describe('Login component', () => {
         expect(history.location.pathname).toBe('/')
     })
 
-    test('Should add accessToken if SaveAccessToken fails', async () => {
+    test('Should present error if SaveAccessToken fails', async () => {
         const { sut, saveAccessTokenMock } = makeSut();
         const error = new InvalidCredentialsError();
         jest.spyOn(saveAccessTokenMock, 'save').mockReturnValueOnce(Promise.reject(error));
