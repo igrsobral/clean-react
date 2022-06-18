3a26449c2bfbe6e0155bec175ae4a6cde217cc91
igorWebMASTER <igor_1917@hotmail.com>
2022-06-18 11:51:23 -0300

    test: ensure Login begin with correct initial state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -4,6 +4,13 @@ describe('Login', () => {
     })
 
     it('should load with correct initial state', () => {
-        cy.get('[data-testid="email-status"]').should('have.attr', 'title', 'Campo obrigatório');
+        cy.get('[data-testid="email-status"]')
+            .should('have.attr', 'title', 'Campo obrigatório')
+            .should('contain.text', '🔴')
+        cy.get('[data-testid="password-status"]')
+            .should('have.attr', 'title', 'Campo obrigatório')
+            .should('contain.text', '🔴')
+        cy.get('[data-testid="submit"]').should('have.attr', 'disabled');
+        cy.get('[data-testid="error-wrap"]').should('not.have.descendants');
     });
 })
