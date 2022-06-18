f59c2f010c21742921773974898f5027f40c165b
igorWebMASTER <igor_1917@hotmail.com>
2022-06-18 12:04:53 -0300

    test: ensure Login present valid state if form is valid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -31,4 +31,17 @@ describe('Login', () => {
         cy.get('[data-testid="submit"]').should('have.attr', 'disabled');
         cy.get('[data-testid="error-wrap"]').should('not.have.descendants');
     });
+
+    it('should present valid state if form is valid', () => {
+        cy.get('[data-testid="email"]').focus().type(faker.internet.email());
+        cy.get('[data-testid="email-status"]')
+            .should('have.attr', 'title', 'Tudo certo')
+            .should('contain.text', '✅')
+        cy.get('[data-testid="password"]').focus().type(faker.random.alphaNumeric(5));
+        cy.get('[data-testid="password-status"]')
+            .should('have.attr', 'title', 'Tudo certo')
+            .should('contain.text', '✅')
+        cy.get('[data-testid="submit"]').should('not.have.attr', 'disabled');
+        cy.get('[data-testid="error-wrap"]').should('not.have.descendants');
+    });
 })
