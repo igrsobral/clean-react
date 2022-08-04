db4fcf64042996a1ed5c2b5c967e797b606e8eae
igorWebMASTER <igor_1917@hotmail.com>
2022-08-03 23:06:57 -0300

    test: ensure SignUp prevent submit if form is invalid

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -98,5 +98,10 @@ describe('Login', () => {
         FormHelper.testHttpCallCount(1)
     });
 
+    it('Should not call submit if form is invalid', () => {
+        Http.mockOk();
+        cy.getByTestId('email').focus().type(faker.internet.email()).type('{enter}');
+        FormHelper.testHttpCallCount(0)
+    });
 })
 
