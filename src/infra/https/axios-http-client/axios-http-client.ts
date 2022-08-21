22c55387e9c46d922c8b909cf4f78273620154a0
igorWebMASTER <igor_1917@hotmail.com>
2022-08-21 12:31:21 -0300

    feat: ensure AxiosHttpClient calls axios.get with correct values

--- src/infra/https/axios-http-client/axios-http-client.ts
+++ src/infra/https/axios-http-client/axios-http-client.ts
@@ -1,22 +1,21 @@
-import { HttpPostClient, HttpPostParams, HttpResponse } from "@/data/protocols/http";
+import { HttpGetParams, HttpPostClient, HttpPostParams, HttpResponse } from "@/data/protocols/http";
 import axios, { AxiosResponse } from 'axios';
 
-export class AxiosHttpClient implements HttpPostClient<any> {
+export class AxiosHttpClient implements HttpPostClient {
     async post(params: HttpPostParams): Promise<HttpResponse> {
         let axiosResponse: AxiosResponse
         try {
             axiosResponse = await axios.post(params.url, params.body);
-            return {
-                statusCode: axiosResponse.status,
-                body: axiosResponse.data
-            };
         } catch (error) {
             axiosResponse = error.response
         }
-
         return {
             statusCode: axiosResponse.status,
             body: axiosResponse.data
         };
+    }
+
+    async get(params: HttpGetParams): Promise<void> {
+        await axios.get(params.url);
     }
 }
