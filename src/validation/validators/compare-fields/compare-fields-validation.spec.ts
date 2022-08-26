68b7b9dcea694614c0b4be24e25ccf8a6df5dabc
igorWebMASTER <igor_1917@hotmail.com>
2022-08-25 23:23:25 -0300

    fix: change compareFieldsValidation test to never return equal values

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -2,20 +2,20 @@ import faker from 'faker'
 import { InvalidFieldError } from '../email/invalid-field-error';
 import { CompareFieldsValidation } from './compare-fields-validation';
 
-const makeSut = (field: string, fieldToCompare: string) : CompareFieldsValidation => new CompareFieldsValidation(field, fieldToCompare);
+const makeSut = (field: string, fieldToCompare: string): CompareFieldsValidation => new CompareFieldsValidation(field, fieldToCompare);
 
-describe('CompareFieldsValidation',  ()=> {
+describe('CompareFieldsValidation', () => {
     test('Should return error if compare is invalid', () => {
         const field = faker.database.column();
         const fieldToCompare = faker.database.column();
         const sut = makeSut(field, fieldToCompare);
         const error = sut.validate({
-            [field]: faker.random.word(),
-            [fieldToCompare]: faker.random.word()
+            [field]: faker.random.words(3),
+            [fieldToCompare]: faker.random.words(4)
         });
         expect(error).toEqual(new InvalidFieldError())
     })
-   
+
     test('Should return error falsy if compare is valid', () => {
         const field = faker.database.column();
         const fieldToCompare = faker.database.column();
