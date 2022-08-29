fb1818f4d944a87fe7d0ee8a1f5fee18c1d3b485
igorWebMASTER <igor_1917@hotmail.com>
2022-08-29 00:20:21 -0300

    test: ensure LocalStorageAdapter calls localStorage.getItem calls with corect value

--- src/main/adapters/current-account-adapter.spec.ts
+++ src/main/adapters/current-account-adapter.spec.ts
@@ -1,14 +1,22 @@
-import { setCurrentAccountAdapter } from './current-account-adapter'
+import { getCurrentAccountAdapter, setCurrentAccountAdapter } from './current-account-adapter'
 import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter'
 import { mockAccountModel } from '@/domain/test'
 import { UnexpectedError } from '@/domain/errors'
 
 describe('CurrentAccountAdapter', () => {
-    test('should call LocalStorageAdapter with correct values', () => {
+    test('should call LocalStorageAdapter.set with correct values', () => {
         const account = mockAccountModel()
         const setSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set')
         setCurrentAccountAdapter(account)
         expect(setSpy).toHaveBeenCalledWith('account', account)
+    })
+
+    test('should call LocalStorageAdapter.get with correct value', () => {
+        const account = mockAccountModel()
+        const getSpy = jest.spyOn(LocalStorageAdapter.prototype, 'get').mockReturnValueOnce(account)
+        const result = getCurrentAccountAdapter()
+        expect(getSpy).toHaveBeenCalledWith('account')
+        expect(result).toEqual(account)
     })
 
     test('should throw UnexpectedError', () => {
