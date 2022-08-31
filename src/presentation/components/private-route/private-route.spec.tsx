e93cf32efba93668294cb12a58b3c26ed7506bec
igorWebMASTER <igor_1917@hotmail.com>
2022-08-31 00:55:41 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/components/private-route/private-route.spec.tsx
+++ src/presentation/components/private-route/private-route.spec.tsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import { createMemoryHistory } from 'history'
+import { createMemoryHistory, MemoryHistory } from 'history'
 import { Route, Router } from 'react-router-dom'
 import PrivateRoutes from './private-route'
 
+type SutTypes = {
+    history: MemoryHistory
+}
+
+const makeSut = (): SutTypes => {
+    const history = createMemoryHistory({ initialEntries: ['/'] })
+    render(
+        <Router history={history}>
+            <PrivateRoutes />
+        </Router>
+    )
+    return { history }
+}
+
 describe('PrivateRoutes', () => {
     test('Should redirect to login if token is empty', () => {
-        const history = createMemoryHistory({ initialEntries: ['/'] })
-        render(
-            <Router history={history}>
-                <PrivateRoutes />
-            </Router>
-        )
+        const { history } = makeSut()
         expect(history.location.pathname).toBe('/login')
     })
 })
