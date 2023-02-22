075e28fe5b3289b7fe84ae98d20c1a50fd2c2696
igorWebMASTER <igor_1917@hotmail.com>
2023-02-21 23:31:40 -0300

    feat: ensure SurveyItem render error on failure

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -13,11 +13,13 @@ type Props = {
 export default function SurveyList({ loadSurveyList }: Props) {
     const [state, setState] = useState({
         surveys: [] as SurveyModel[],
+        error: ''
     })
 
     useEffect(() => {
         loadSurveyList.loadAll()
-            .then(surveys => setState({ surveys }))
+            .then(surveys => setState({ ...state, surveys }))
+            .catch(error => setState({ ...state, error: error.message }))
     }, [])
     
     return (
@@ -25,11 +27,18 @@ export default function SurveyList({ loadSurveyList }: Props) {
             <Header />
             <div className={Styles.contentWrap}>
                 <h2>Enquetes</h2>
+                {state.error 
+                ? <div>
+                    <span data-testid="error">{state.error}</span>
+                    <button>Recarregar</button>
+                </div>
+                :  
                 <ul data-testid="survey-list">
                     {state.surveys.length 
                     ? state.surveys.map((survey) => <SurveyItem key={survey.id} survey={survey} />)
                     : <SurveyItemEmpty /> }
                 </ul>
+                }
             </div>
             <Footer />
         </div>
