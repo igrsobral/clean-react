48adfc45cc654f278d1d108a2e1d34601dfb0c44
igorWebMASTER <igor_1917@hotmail.com>
2023-02-21 23:22:08 -0300

    test: ensure SurveyItem load SurveyItems on success

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -1,6 +1,6 @@
 import { LoadSurveyList } from '@/domain/useCases';
 import { SurveyList } from "@/presentation/pages";
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import { BrowserRouter } from "react-router-dom";
 import { SurveyModel } from '@/domain/models';
 import React from 'react'
@@ -33,20 +33,23 @@ const makeSut = (): SutTypes => {
 }
 
 describe('SurveyList Component', () => {
-    test('Should present 4 empty items on start', () => {
+    test('Should present 4 empty items on start', async () => {
         makeSut()
         const surveyList = screen.getByTestId('survey-list')
-        expect(surveyList.querySelectorAll('li:empty').length).toBe(4)
+        expect(surveyList.querySelectorAll('li:empty')).toHaveLength(4)
+        await waitFor(() => surveyList)
     })
    
-    test('Should call LoadSurveyList', () => {
+    test('Should call LoadSurveyList', async () => {
         const { loadSurveyListSpy } = makeSut()
         expect(loadSurveyListSpy.callsCount).toBe(1)
+        await waitFor(() => screen.getByRole('heading'))
     })
     
-    test('Should render SurveyItem on success', () => {
+    test('Should render SurveyItem on success', async () => {
         makeSut()
         const surveyList = screen.getByTestId('survey-list')
-        expect(surveyList.querySelectorAll('li').length).toBe(4)
+        await waitFor(() => surveyList)
+        expect(surveyList.querySelectorAll('li')).toHaveLength(4)
     })
 })
