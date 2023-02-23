4fd6bfa7b0e6ddfa9d2a73ed6b9685fb592e99e0
igorWebMASTER <igor_1917@hotmail.com>
2023-02-22 23:33:10 -0300

    feat: ensure SurveyList calls LoadSurveyList on reload click

--- src/presentation/pages/survey-list/components/error/error.tsx
+++ src/presentation/pages/survey-list/components/error/error.tsx
@@ -3,11 +3,20 @@ import Styles from './error-styles.scss'
 import { SurveyContext } from '@/presentation/pages/survey-list/components'
 
 export default function Error() {
-  const { state } = useContext(SurveyContext)
+  const { state, setState } = useContext(SurveyContext)
+
+  const reload = () => {
+    setState({ surveys: [], error: '', reload: !state.reload  })
+  }
   return (
     <div className={Styles.errorWrap}>
         <span data-testid="error">{state.error}</span>
-        <button>Recarregar</button>
+        <button 
+          data-testid="reload"
+          onClick={reload}
+        >
+          Tentar novamente
+        </button>
     </div>
   )
 }
