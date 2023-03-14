2596b6955fdc17c3f2b388673428e1d10be33522
Igor Ribeiro <igor_1917@hotmail.com>
2023-03-14 23:58:41 +0000

    feat: add makeAuthorizeHttpGetClientDecorator factory

--- src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
+++ src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
@@ -1,8 +1,8 @@
-import { LoadSurveyList } from "@/domain/useCases";
-import { makeAxiosHttpClient } from "@/main/factories/http/axios-main-client-factory";
 import { makeApiUrl } from "@/main/factories/http/api-url-factory";
+import { makeAuthorizeHttpGetClientDecorator } from "@/main/factories/decorators";
+import { LoadSurveyList } from "@/domain/useCases";
 import { RemoteLoadSurveyList } from "@/data/usecases/load-survey-list/remote-load-survey-list";
 
 export const makeRemoveLoadSurveyList = () : LoadSurveyList => {
-    return new RemoteLoadSurveyList(makeApiUrl('/surveys'), makeAxiosHttpClient())
+    return new RemoteLoadSurveyList(makeApiUrl('/surveys'), makeAuthorizeHttpGetClientDecorator())
 }
