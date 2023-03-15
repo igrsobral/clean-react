eb0c7fec2b5dab3f70c8f4a9c9df6c9b61819272
igorWebMASTER <igor_1917@hotmail.com>
2023-03-15 15:51:25 -0300

    chore: update cypress to latest version

---
+++ cypress.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  fixturesFolder: false,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./src/main/test/cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://localhost:8080',
+    supportFile: 'src/main/test/cypress/support/index.js',
+    specPattern: 'src/main/test/cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
+  },
+})
